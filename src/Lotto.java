b47d1320f63b01dc61b4f7d6336cd40445ffef69
ksr0818 <ksr940818@gmail.com>
2023-01-05 21:33:14 +0900

    로또

--- src/Lotto.java
+++ src/Lotto.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Lotto {
+import java.util.HashSet;
+
+public class Lotto {
+
+    public static void main(String[] args) {
+        HashSet<Integer> lotto = new HashSet<>();
+        for (int i=1; lotto.size() < 6; i++){
+            int num = (int) (Math.random() * 45) +1;
+            System.out.printf("%d번째 번호는 %d입니다.\n",i,num);
+            lotto.add(num);
+        }
+        System.out.println(lotto);
+    }
 }
